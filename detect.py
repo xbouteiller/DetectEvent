debf32085a2c3e72a0ee2d967cc4a830f552ac98
xavier bouteiller <bouteiller.xavier@gmail.com>
2020-11-09 09:44:43 +0100

    added smoother

--- detect.py
+++ detect.py
@@ -2,6 +2,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from scipy import signal
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel, RationalQuadratic, ExpSineSquared
+from loess.loess_1d import loess_1d
+from statsmodels.nonparametric.smoothers_lowess import lowess
 
 # %%
 def create_exp(N, A, B):
@@ -35,7 +40,6 @@ def sliding_window_pred(X, y, window, lag):
     score = [fit_and_pred(X[s:s+window], y[s:s+window]) 
             for s in start]    
     return score
-
 
 # %% 
 expo = create_exp(N = 15000, A = 10 , B = 0.0001)
@@ -170,3 +174,250 @@ plt.show()
 
 plt.scatter(np.arange(0,np.shape(np.diff(score))[0]),np.diff(score)-np.mean(np.diff(score)))
 plt.show()
+
+
+# %%
+X, Y = concat_noise(C = [expo,lin], m = 0, s = 0.8)
+Ysmooth = signal.savgol_filter(Y, 53, 1)
+
+ # %%
+plt.scatter(X,Y)
+plt.scatter(X,Ysmooth)
+plt.show()
+
+plt.scatter(X[0:1000],Y[0:1000])
+plt.scatter(X[0:1000],Ysmooth[0:1000])
+plt.show()
+
+# %%
+score = sliding_window_pred(X = X, y = Y, 
+                            window = 5877, 
+                            lag = 1279)
+plt.bar(np.arange(0,np.shape(score)[0]),score)
+plt.show()
+plt.scatter(np.arange(0,np.shape(np.diff(score))[0]),np.diff(score))
+plt.show()
+
+
+score = sliding_window_pred(X = X, y = Ysmooth, 
+                            window = 5877, 
+                            lag = 1279)
+plt.bar(np.arange(0,np.shape(score)[0]),score)
+plt.show()
+plt.scatter(np.arange(0,np.shape(np.diff(score))[0]),np.diff(score))
+plt.show()
+
+# %%
+
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    
+    
+    https://scipy.github.io/old-wiki/pages/Cookbook/SignalSmooth.html
+    """
+
+    if x.ndim != 1:
+        raise ValueError("smooth only accepts 1 dimension arrays.")
+
+    if x.size < window_len:
+        raise ValueError("Input vector needs to be bigger than window size.")
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError("Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
+
+
+#%%
+X, Y = concat_noise(C = [expo,lin], m = 0, s = 0.8)
+Ysmooth = smooth(Y,window_len=150,window='hamming')[0:30000]
+
+
+ # %%
+plt.scatter(X,Y)
+plt.scatter(X,Ysmooth)
+plt.show()
+
+plt.scatter(X[0:1000],Y[0:1000])
+plt.scatter(X[0:1000],Ysmooth[0:1000])
+plt.show()
+
+# %%
+score = sliding_window_pred(X = X, y = Y, 
+                            window = 5877, 
+                            lag = 1279)
+plt.bar(np.arange(0,np.shape(score)[0]),score)
+plt.show()
+plt.scatter(np.arange(0,np.shape(np.diff(score))[0]),np.diff(score))
+plt.show()
+
+
+score = sliding_window_pred(X = X, y = Ysmooth, 
+                            window = 5877, 
+                            lag = 1279)
+plt.bar(np.arange(0,np.shape(score)[0]),score)
+plt.show()
+plt.scatter(np.arange(0,np.shape(np.diff(score))[0]),np.diff(score))
+plt.show()
+
+
+# ## Loess smoothing
+
+# In[117]:
+
+
+X, Y = concat_noise(C = [expo,lin], m = 0, s = 0.8)
+
+# delta & frac : hyperparameters for adjusting smoothing
+
+Ysmooth = lowess(exog = X, endog = Y, frac = 0.01, delta = 0.001 * X.shape[0], return_sorted = False)
+
+
+# In[118]:
+
+
+plt.scatter(X,Y)
+plt.scatter(X,Ysmooth)
+plt.show()
+
+plt.scatter(X[0:1000],Y[0:1000])
+plt.scatter(X[0:1000],Ysmooth[0:1000])
+plt.show()
+
+
+# In[119]:
+
+
+# %% 
+score = sliding_window_pred(X = X, y = Y, 
+                            window = 5877, 
+                            lag = 1279)
+plt.bar(np.arange(0,np.shape(score)[0]),score)
+plt.title('No smoothing')
+plt.show()
+plt.scatter(np.arange(0,np.shape(np.diff(score))[0]),np.diff(score))
+plt.title('No smoothing')
+plt.show()
+
+
+# In[120]:
+
+
+score = sliding_window_pred(X = X, y = Ysmooth, 
+                            window = 6587, 
+                            lag = 1279)
+plt.bar(np.arange(0,np.shape(score)[0]),score)
+plt.title('With smoothing')
+plt.show()
+plt.scatter(np.arange(0,np.shape(np.diff(score))[0]),np.diff(score))
+plt.title('With smoothing')
+plt.show()
+
+
+# ## Gaussian Process Regressor
+
+# **NB : Not working yet, need to find the optimal kernel**
+
+# In[ ]:
+
+
+# %%
+X, Y = concat_noise(C = [expo,lin], m = 0, s = 0.8)
+k1 = 1.0*RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0))  # long term smooth rising trend
+k4 = WhiteKernel(noise_level=1e-1)  # noise terms
+kernel_gpml = k1 + k4
+gpr = GaussianProcessRegressor(kernel = kernel_gpml, random_state=0).fit(X.reshape(-1,1), Y.reshape(-1,1))
+Ysmooth= gpr.predict(X.reshape(-1,1), return_std=True)
+
+
+# In[ ]:
+
+
+# %%
+plt.scatter(X,Y)
+plt.scatter(X,Ysmooth)
+plt.show()
+
+plt.scatter(X[0:1000],Y[0:1000])
+plt.scatter(X[0:1000],Ysmooth[0:1000])
+plt.show()
+
+
+# In[ ]:
+
+
+# %%
+score = sliding_window_pred(X = X, y = Y, 
+                            window = 5877, 
+                            lag = 1279)
+plt.bar(np.arange(0,np.shape(score)[0]),score)
+plt.show()
+plt.scatter(np.arange(0,np.shape(np.diff(score))[0]),np.diff(score))
+plt.show()
+
+
+# In[ ]:
+
+
+score = sliding_window_pred(X = X, y = Ysmooth, 
+                            window = 5877, 
+                            lag = 1279)
+plt.bar(np.arange(0,np.shape(score)[0]),score)
+plt.show()
+plt.scatter(np.arange(0,np.shape(np.diff(score))[0]),np.diff(score))
+plt.show()
+
+
+# # Other useful methods
+# 
+# https://link.medium.com/hGdVrrxPabb
+# 
+# https://github.com/arvkevi/kneed
+# 
+# https://stackoverflow.com/questions/28269379/curve-curvature-in-numpy
+# 
+# https://iescoders.com/calculating-the-curvature-of-a-curve/
+# 
+# https://github.com/Pjer-zhang/PJCurvature/blob/master/src/py/PJcurvature_demo.ipynb
